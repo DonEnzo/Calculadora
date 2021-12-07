078c3f9512fc280cdd45aee717a569af8e94204f
Enzo Bazan <92554152+DonEnzo@users.noreply.github.com>
2021-12-07 19:56:12 -0300

    Add files via upload

---
+++ Calculadora-JS/Calcapp.js
@@ -0,0 +1,19 @@
+// este js se inicia con caps porque tiene clase y por convencion los archivos con clase se inician con mayuscula
+// la clase se declara con class y el nombre  + llaves correspondientes
+class calculadora {
+    sumar(num1, num2) {
+        return num1 + num2;
+    }
+
+    restar(num1, num2) {
+        return num1 - num2;
+    }
+
+    dividir(num1, num2) {
+        return num1 / num2;
+    }
+
+    multiplicar(num1, num2) {
+        return num1 * num2;
+    }
+}
---
+++ Calculadora-JS/Display.js
@@ -0,0 +1,53 @@
+class Display {
+    constructor(displayValorAnterior, displayValorActual) {
+        this.displayValorActual = displayValorActual;
+        this.displayValorAnterior = displayValorAnterior;
+        this.calculadora = new calculadora();
+        this.valorActual = '';
+        this.valorAnterior = '';
+        {}
+    }
+
+
+    borrar() {
+        this.valorActual = this.valorActual.toString().slice(0,-1);
+        this.imprimirValores();
+        this.imprimirValores();
+    }
+
+
+    borrarTodo() {
+        this.valorActual = '';
+        this.valorAnterior = '';
+        this.tipoOperacion = undefined;
+
+    }
+
+
+    computar(tipo) {
+        this.tipoOperacion !== 'igual' && this.calcular();
+        this.tipoOperacion = tipo;
+        this.valorAnterior = this.valorActual || this.valorAnterior;
+        this.valorActual = '';
+        this.imprimirValores();
+    }
+
+    agregarNumero(numero) {
+        if(numero === '.' && this.valorActual.includes('.')) return
+        this.valorActual = this.valorActual.toString() + numero;
+        this.imprimirValores();
+    }
+
+
+    imprimirValores() {
+        this.displayValorActual.textContent = this.ValorActual;
+        this.displayValorAnterior.textContent = `${this.valorAnterior} ${this.signos}`;
+    }
+
+    calcular() {
+        const valorAnterior = parseFloat(this.valorAnterior);
+        const valorActual = parseFloat(this.valorActual);
+
+        if( isNaN(valorActual) || isNaN(valorAnterior) ) return this.valorActual = this.calculadora[this.tipoOperacion](valorAnterior, valorActual)
+    }
+}
